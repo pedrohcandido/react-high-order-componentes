c9e1e9dc70596b9eb6de028c375e7eadaedf8c94
Pedro Candido <pedroh_candido@hotmail.com>
2024-06-05 17:13:13 -0300

    alteracoes-componente-de-interface-envios-formulario

--- src/components/formAnswer/FormAnswer.jsx
+++ src/components/formAnswer/FormAnswer.jsx
@@ -1,36 +1,12 @@
 import React from "react";
 import './FormAnswer.css';
 
-const FormAnswer = (props) => {
-	return(
-		<div className="app2">
-			<table>
-				<tr>
-					<td>Nome: <p id="name"> {props._name} </p> </td>
-				</tr>
-				<tr></tr><tr></tr>
-				<tr>
-					<td>Idade: <p id="age"> {props._age} </p> </td>
-				</tr>
-				<tr></tr><tr></tr>
-				<tr>
-					<td>Sexo: <p id="gender"> {props._gender} </p> </td>
-				</tr>
-				<tr></tr><tr></tr>
-				<tr>
-					<td>Estado Civil: <p id="civilStatus"> {props._civilStatus} </p> </td>
-				</tr>
-				<tr></tr><tr></tr>
-				<tr>
-					<td>Tipo de Documento: <p id="documentType"> {props._docType} </p> </td>
-				</tr>
-				<tr></tr><tr></tr>
-				<tr>
-					<td>CPF/CNPJ: <p id="document"> {props._document}</p> </td>
-				</tr>
-			</table>
-		</div>
-	)
+class FormAnswer extends React.Component {
+	render() {
+		return(
+				<h3>{this.props.name}</h3>
+		)
+	}	
 }
 
 export default FormAnswer;
