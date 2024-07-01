6879bf71a0b9f0631b963baeffe314cd6e5c4c6d
Pedro Candido <pedroh_candido@hotmail.com>
2024-07-01 13:48:20 -0300

    adicionando-segundo-formulario-appjs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Form from './components/form';
+import Form2 from './components/form2';
 
 function App() {
   return (
+		<>
 			<Form></Form>
+			<Form2></Form2>
+		</>
   );
 }
 
